880feac5c23d41f640274261eab1db02d2e02a04
MarceloItada <pedro.arautos13@gmail.com>
2026-02-23 12:25:15 -0300

    atividade 01 recalculo de salário

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+
+const host = '0.0.0.0';
+const porta = 3000;
+
+const server = express(); //oferecendo ao desenvolvedor um servidor http de modo expresso
+
+//recheando servidor com funcionalidades
+
+server. get('/', (requisicao, resposta) => {
+    resposta.send(`
+        <DOCTYPE html>
+        <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Atividade 01 programa para internet usando node + express</title>
+            </head>
+            <body>
+                <h1>Atividade 01 programa para internet usando node + express</h1>
+                <h2>Olá, bem-vindo a página inicial</h2>
+            </body>
+            </html>
+
+        `);
+});
+
+server.get('/horaAtual', (requisicao, resposta) => {
+    const horaAtual = new Date();
+    const hora = horaAtual.getHours() + ":" + horaAtual.getMinutes() + ":" + horaAtual.getSeconds();
+    resposta.send(`
+        <DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Horário do servidor</title>
+         </head>
+         <body>
+            <h1>Agora são ${hora}</h1>
+         </body>
+         </html>
+            
+    `);
+});
+
+server.listen(porta, host, () => {
+    console.log(`Servidor escutando em http://${host}:${porta}`);
+});
